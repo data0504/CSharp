035426fedcb060ce7cb15bd7c1d7de16cf876982
Data <110648069+data0504@users.noreply.github.com>
2023-06-05 23:55:28 +0800

    Update ButterflyLogic.cs

--- Unity/ButterflyLogic.cs
+++ Unity/ButterflyLogic.cs
@@ -4,12 +4,8 @@ using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-
-
-public class ButterflyLogic : MonoBehaviour
+public class Butterfly
 {
-    public GameObject View_bullet;
-    public RectTransform  View_bulletRect;
 
     private readonly float pointX = 50f;
     private readonly float pointY = 500f;
@@ -17,18 +13,19 @@ public class ButterflyLogic : MonoBehaviour
     private readonly float addPointX = 5f;
 
     private float row = 1f;
+    private float timer = 0f;
+    private readonly float interval = 0.075f;
+    private float timeGap;
 
 
     private GameObject Model_bullet;
     private Transform Model_bullet_Transfrom;
 
 
-
-    void Start()
+    public void Init(GameObject view_bullet, RectTransform view_bulletRect)
     {
-        Model_bullet = Instantiate(View_bullet, View_bulletRect);
+        Model_bullet = GameObject.Instantiate(view_bullet, view_bulletRect);
         Model_bullet_Transfrom = Model_bullet.GetComponent<Transform>();
-        InvokeRepeating(nameof(Fly), 0.0000001f, 0.075f);
     }
     private void Fly()
     {
@@ -37,9 +34,31 @@ public class ButterflyLogic : MonoBehaviour
         Model_bullet_Transfrom.position = new Vector3(pointXIng, pointYIng, pointZ);
     }
 
-    void Update()
+    public void ParserTime(float deltaTime)
     {
-        //not logic
+        timer += deltaTime;
+        if (timer >= interval)
+        {
+            Fly();
+            timeGap = interval - timer;
+            timer = timeGap;
+        }
+    }
+}
+
+public class ButterflyLogic : MonoBehaviour
+{
+    public GameObject View_bullet; 
+    public RectTransform View_bulletRect; 
+    readonly Butterfly butterfly = new();
+
+    void Start()
+    {
+        butterfly.Init(View_bullet, View_bulletRect);
     }
 
+    void Update()
+    {
+        butterfly.ParserTime(Time.deltaTime);
+    }
 }
